33a373c35f3d9f7a19276cc3591b3d0a2be9f417
jihunsuh <jihunsuh12@gmail.com>
2023-08-23 17:42:22 +0900

    Header 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,40 +5,20 @@ export default function Header() {
   return (
     <div className="w-full flex justify-between max-w-7xl mx-auto border-b border-[#333]">
       {/* header left */}
-      <div className="flex space-x-[30px] py-[10px] items-center">
-        {/* <div className=" font-bold pr-[15px]">POTTERY</div> */}
+      <div className="flex space-x-[30px] py-[10px] items-center text-[13px] font-medium leading-4 text-[#333]">
         <Image src={PotteryLogo} alt="" className="w-[75px] pr-[15px]" />
-
-        <div className="text-[13px] font-medium leading-4 text-[#333]">
-          스토어
-        </div>
-        <div className="text-[13px] font-medium leading-4 text-[#333]">
-          셔츠
-        </div>
-        <div className="text-[13px] font-medium leading-4 text-[#333]">
-          데님
-        </div>
-        <div className="text-[13px] font-medium leading-4 text-[#333]">
-          셋업
-        </div>
-        <div className="text-[13px] font-medium leading-4 text-[#333]">
-          가이드
-        </div>
+        <div>스토어</div>
+        <div>셔츠</div>
+        <div>데님</div>
+        <div>셋업</div>
+        <div>가이드</div>
       </div>
       {/* header right */}
-      <div className="flex space-x-[30px] py-[10px] items-center">
-        <div className="text-[13px] font-medium leading-4 text-[#333]">
-          검색
-        </div>
-        <div className="text-[13px] font-medium leading-4 text-[#333]">
-          로그인
-        </div>
-        <div className="text-[13px] font-medium leading-4 text-[#333]">
-          쇼핑백
-        </div>
-        <div className="text-[13px] font-medium leading-4 text-[#333]">
-          고객지원
-        </div>
+      <div className="flex space-x-[30px] py-[10px] items-center text-[13px] font-medium leading-4 text-[#333]">
+        <div>검색</div>
+        <div>로그인</div>
+        <div>쇼핑백</div>
+        <div>고객지원</div>
       </div>
     </div>
   );
